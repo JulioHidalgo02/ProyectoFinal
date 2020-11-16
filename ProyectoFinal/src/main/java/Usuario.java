3d27ed32ae9b1eb6b37b08a481835c8dd8ff7fc0
Julitoxcr3 <72364230+Julitoxcr3@users.noreply.github.com>
2020-11-15 18:52:29 -0600

    Agregue dos nuevas clases Usuario y Estados. Ademas agregue el menu principal y los atributos de la clase usuario

---
+++ ProyectoFinal/src/main/java/Usuario.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Usuario {
+
+    
+    private String nombre;
+    private String apellidos;
+    private String cedula;
+    private String correoElectronico;
+    private String contrasenia;
+    private String numCelular;
+    
+    public Usuario() {
+        nombre = "";
+        apellidos = "";
+        cedula = "";
+        correoElectronico = "";
+        contrasenia = "";
+        numCelular = "";
+          
+    }
+}
+
+
