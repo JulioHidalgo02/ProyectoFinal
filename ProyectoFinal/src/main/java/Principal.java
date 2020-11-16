9941848936f5882f57866d3c7aa5accce405fb56
Julitoxcr3 <72364230+Julitoxcr3@users.noreply.github.com>
2020-11-16 13:13:08 -0600

    Modifique unas cosas que estaban mal en el menu

--- ProyectoFinal/src/main/java/Principal.java
+++ ProyectoFinal/src/main/java/Principal.java
@@ -4,12 +4,27 @@ import javax.swing.JOptionPane;
 public class Principal {
 
     public static void main(String[] args) {
-       
-        int op = Integer.parseInt(JOptionPane.showInputDialog("Digite la opción que desea elegir \n 1- Parques Nacionales \n 2- Volcanes \n 3- Teatros \n 4- Hoteles \n 5- Playas \n 6- Parques Acuaticos y Diversiones"));
+       Menu();
+        
+        
+        
+        
+      
+        
+        
+        
+                
+        
+    }// Fin del Main
+    
+   public static void Menu(){
+      ParquesNacionales parque = new ParquesNacionales();
+       int op; 
         do{
-            
+         op = Integer.parseInt(JOptionPane.showInputDialog("Digite la opción que desea elegir \n 1- Parques Nacionales \n 2- Volcanes \n 3- Teatros \n 4- Hoteles \n 5- Playas \n 6- Parques Acuaticos y Diversiones \n 0- Salir"));   
         switch(op){
             case 1:
+                parque.Menu();
                 
                 break;
                 
@@ -34,28 +49,15 @@ public class Principal {
                 break;
                 
             
+                
+            
         }
         }while (op != 0);
-        
-        
-        
-      
-        
-        
-        
-                
-        
-    }
+         
+   } 
+   
     
-}
+}// Fin de la clase principal
 
 
 
-/*
-nombre = JOptionPane.showInputDialog("Digite su nombre");
-         apellidos = JOptionPane.showInputDialog("Digite sus apellidos");
-         cedula  = JOptionPane.showInputDialog("Digite su cedula");
-          numCelular = JOptionPane.showInputDialog("Digite su numero de celular");
-           correoElectronico = JOptionPane.showInputDialog("Digite su correo electronico");
-            contrasenia = JOptionPane.showInputDialog("Digite una contraseña");
-*/
