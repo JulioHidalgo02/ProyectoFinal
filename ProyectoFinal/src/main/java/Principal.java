6619cfdeaa268cfbf7741b78603b0ece4c1fc5c2
Julitoxcr3 <72364230+Julitoxcr3@users.noreply.github.com>
2020-11-15 19:05:35 -0600

    Menu

--- ProyectoFinal/src/main/java/Principal.java
+++ ProyectoFinal/src/main/java/Principal.java
@@ -1,24 +1,46 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author WPC
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+       
+        int op = Integer.parseInt(JOptionPane.showInputDialog("Digite la opción que desea elegir \n 1- Parques Nacionales \n 2- Volcanes \n 3- Teatros \n 4- Hoteles \n 5- Playas \n 6- Parques Acuaticos y Diversiones"));
+        do{
+            
+        switch(op){
+            case 1:
+                
+                break;
+                
+            case 2:
+                
+                break;
+                
+            case 3:
+                
+                break;
+                
+            case 4:
+                
+                break;
+                
+            case 5: 
+                
+                break;
+                
+            case 6:
+                
+                break;
+                
+            
+        }
+        }while (op != 0);
+        
+        
+        
       
-        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        
         
         
                 
@@ -26,3 +48,14 @@ public class Principal {
     }
     
 }
+
+
+
+/*
+nombre = JOptionPane.showInputDialog("Digite su nombre");
+         apellidos = JOptionPane.showInputDialog("Digite sus apellidos");
+         cedula  = JOptionPane.showInputDialog("Digite su cedula");
+          numCelular = JOptionPane.showInputDialog("Digite su numero de celular");
+           correoElectronico = JOptionPane.showInputDialog("Digite su correo electronico");
+            contrasenia = JOptionPane.showInputDialog("Digite una contraseña");
+*/
