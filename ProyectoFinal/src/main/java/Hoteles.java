935ba1d3b2f107f4cb8e00a97a6fb3190522f44e
Julitoxcr3 <72364230+Julitoxcr3@users.noreply.github.com>
2020-11-19 13:09:40 -0600

    Correccion de errores

--- ProyectoFinal/src/main/java/Hoteles.java
+++ ProyectoFinal/src/main/java/Hoteles.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author WPC
- */
+import javax.swing.JOptionPane;
 public class Hoteles {
     Usuario espacios[] = new Usuario[10];
    
