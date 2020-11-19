48278a882f2cbd3d04341647cedac2bf0d3f5004
Julitoxcr3 <72364230+Julitoxcr3@users.noreply.github.com>
2020-11-19 13:08:02 -0600

    Cree los objetos de las clases para que funcionara el programa

--- ProyectoFinal/src/main/java/Principal.java
+++ ProyectoFinal/src/main/java/Principal.java
@@ -7,18 +7,16 @@ public class Principal {
        Menu();
         
         
-        
-        
-      
-        
-        
-        
-                
-        
+    
     }// Fin del Main
     
    public static void Menu(){
       ParquesNacionales parque = new ParquesNacionales();
+      Hoteles hoteles = new Hoteles();
+      Volcanes volcanes = new Volcanes();
+      Teatros teatros = new Teatros();
+      Parques_Acuaticos_Diversiones parqueAD = new Parques_Acuaticos_Diversiones();
+      Playas playas = new Playas();
        int op; 
         do{
          op = Integer.parseInt(JOptionPane.showInputDialog("Digite la opción que desea elegir \n 1- Parques Nacionales \n 2- Volcanes \n 3- Teatros \n 4- Hoteles \n 5- Playas \n 6- Parques Acuaticos y Diversiones \n 0- Salir"));   
@@ -29,22 +27,26 @@ public class Principal {
                 break;
                 
             case 2:
-                
+                volcanes.Menu();
                 break;
                 
             case 3:
+                teatros.Menu();
                 
                 break;
                 
             case 4:
+                hoteles.Menu();
                 
                 break;
                 
             case 5: 
-                
+                playas.Menu();
                 break;
                 
             case 6:
+                
+               parqueAD.Menu();
                 
                 break;
                 
